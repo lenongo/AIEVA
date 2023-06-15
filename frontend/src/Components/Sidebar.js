223cb567e991ca7fcc9a75ea71198f8faeec8b97
lenongo <funakki0904@gmail.com>
2023-06-16 06:27:10 +0900

    create frontend

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,6 +1,8 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Link, Image } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
   return (
     <Box
       bg="cyan.500"
@@ -10,8 +12,35 @@ const Sidebar = () => {
       justifyContent="space-between"
       alignItems="center"
     >
+      <Image
+        ml="2.5vw"
+        width="15vw"
+        src="https://bit.ly/dan-abramov"
+        alt="Dan Abramov"
+      />
       {/* ヘッダーのコンテンツを追加 */}
-      <Button mt="3vh">Wallet Connect</Button>
+      <Button
+        bg="white"
+        color="black"
+        mt="3vh"
+        width="15vw"
+        onClick={() => navigate('/')}
+      >
+        Top
+      </Button>
+      <Button bg="white" color="black" mt="3vh" width="15vw">
+        Task Review
+      </Button>
+
+      <Button
+        bg="white"
+        color="black"
+        mt="3vh"
+        width="15vw"
+        onClick={() => navigate('/receive')}
+      >
+        Receive
+      </Button>
     </Box>
   );
 };
